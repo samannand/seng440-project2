27f98f3abea9b477b846616c651a94270b750743
Sam Annand <sga111@uclive.ac.nz>
2020-05-25 21:30:52 +1200

    Created snapchat vector icon asset and multi resolution launcher icons

--- sources/Crapchat/app/src/main/java/sga111/seng440/crapchat/ui/map/MapFragment.kt
+++ sources/Crapchat/app/src/main/java/sga111/seng440/crapchat/ui/map/MapFragment.kt
@@ -8,7 +8,6 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import sga111.seng440.crapchat.R
 
 class MapFragment : Fragment() {
