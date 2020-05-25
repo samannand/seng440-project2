5a39fc41f8a00a3e57949755e60f2e6a2e8409c2
Sam Annand <sga111@uclive.ac.nz>
2020-05-26 00:08:21 +1200

    Beginning camera implementation

--- sources/Crapchat/app/src/main/java/sga111/seng440/crapchat/ui/camera/CameraFragment.kt
+++ sources/Crapchat/app/src/main/java/sga111/seng440/crapchat/ui/camera/CameraFragment.kt
@@ -1,19 +1,39 @@
 package sga111.seng440.crapchat.ui.camera
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
+import android.os.Environment
+import android.view.*
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.Camera
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import sga111.seng440.crapchat.R
+import java.io.File
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class CameraFragment : Fragment() {
 
     private lateinit var cameraViewModel: CameraViewModel
+    private var preview: Preview? = null
+    private var imageCapture: ImageCapture? = null
+    private var imageAnalyzer: ImageAnalysis? = null
+    private var camera: Camera? = null
+
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+    private lateinit var cameraCaptureButton: Button
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -22,6 +42,72 @@ class CameraFragment : Fragment() {
     ): View? {
         cameraViewModel = ViewModelProvider(this).get(CameraViewModel::class.java)
         val view = inflater.inflate(R.layout.fragment_camera, container, false)
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                activity as Activity, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        // Setup the listener for take photo button
+        cameraCaptureButton = view.findViewById(R.id.camera_capture_button)
+        cameraCaptureButton.setOnClickListener { takePhoto() }
+
+        outputDirectory = getOutputDirectory()!!
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
         return view
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(context, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun startCamera() {
+        // TODO
+    }
+
+    private fun takePhoto() {
+        // TODO
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            context!!, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+
+
+//    fun getOutputDirectory(): File? {
+//        val mediaDir = Environment.getExternalStorageDirectory()?.let {
+//            File(it, resources.getString(R.string.app_name)).apply { mkdirs() } }
+//        return if (mediaDir != null && mediaDir.exists())
+//            mediaDir else null // this used to say filesDir
+//    }
+
+    fun getOutputDirectory(): File {
+        return File(Environment.getExternalStorageDirectory(), resources.getString(R.string.app_name)).apply { mkdirs() }
+    }
+
+    companion object {
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     }
 }
